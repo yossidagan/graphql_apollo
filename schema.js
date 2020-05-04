88f9ab0e428a9dc7e832133abb7b6a9518455948
yossi dagan <dagangm@gmail.com>
2020-05-05 00:03:21 +0300

    added root query

--- schema.js
+++ schema.js
@@ -2,8 +2,14 @@ const {
   GraphQLObjectType,
   GraphQLInt,
   GraphQLString,
-  GraphQLBoolean
+  GraphQLBoolean,
+  GraphQLList,
+  GraphQLSchema
 } = require('graphql')
+
+const axios = require('axios')
+
+// Types
 
 const LaunchType = new GraphQLObjectType({
   name: 'Launch',
@@ -24,4 +30,24 @@ const RocketType = new GraphQLObjectType({
     rocket_name: { type: GraphQLString },
     rocket_type: { type: GraphQLString }
   })
+})
+
+/// Root Query
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    launches: {
+      type: new GraphQLList(LaunchType),
+      resolve(parent, args) {
+        return axios
+          .get('https://api.spacexdata.com/v3/launches')
+          .then((res) => res.data)
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 })
